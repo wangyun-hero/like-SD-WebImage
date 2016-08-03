50b0cc258709c8a2f7c777ed61bbc7c4a35e2572
王云 <wangyun120411@163.com>
2016-08-03 16:47:28 +0800

    用自己的框架完成图片的下载(问题:当我们延迟下载时间的时候,会出现图片错乱)

---
+++ sd加载图片/HMWebImage/UIImageView+HMWebCache.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+HMWebCache.h
+//  sd加载图片
+//
+//  Created by 王云 on 16/8/3.
+//  Copyright © 2016年 王云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (HMWebCache)
+
+@end
---
+++ sd加载图片/HMWebImage/UIImageView+HMWebCache.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+HMWebCache.m
+//  sd加载图片
+//
+//  Created by 王云 on 16/8/3.
+//  Copyright © 2016年 王云. All rights reserved.
+//
+
+#import "UIImageView+HMWebCache.h"
+
+@implementation UIImageView (HMWebCache)
+
+@end
